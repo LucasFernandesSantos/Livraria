34e37c5c834c0134e884a7005b9a1e4a6b387dd8
Lucas Fernandes <lucasfernandes_06@hotmail.com>
2019-11-19 18:13:19 -0300

    Implemntação da classe Endereco,implementação da classe Cliente e criaçao do LayoutHome

--- Livraria/Models/Cliente.cs
+++ Livraria/Models/Cliente.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Livraria.Models
 {
+    [Table("Cliente")]
     public class Cliente
     {
-          
+        [Key]
+        public int IdCliente { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Senha { get; set; }
+        public string DataNascimento { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public Endereco Endereco { get; set; }
     }
 }
