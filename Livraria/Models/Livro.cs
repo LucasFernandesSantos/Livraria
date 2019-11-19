63171c51abae55874064a25808bbd0fab06ee014
Lucas <lucasfernandes_06@hotmail.com>
2019-11-19 09:26:12 -0300

    Inicio do projeto criação das classes e do controller

---
+++ Livraria/Models/Livro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Livraria.Models
+{
+    [Table("Livros")]
+    public class Livro
+    {
+       
+        public int MyProperty { get; set; }
+    }
+}
