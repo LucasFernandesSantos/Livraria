d5e98207adb897dd5a1933e0d2db6f60a18068bd
Gandin13 <gandiboon@gmail.com>
2019-11-20 20:20:19 -0300

    ViaCEP Funcionando

--- Livraria/Startup.cs
+++ Livraria/Startup.cs
@@ -29,12 +29,13 @@ namespace Livraria
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
+                options.CheckConsentNeeded = context => false;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
             services.AddScoped<LivroDAO>();
             services.AddScoped<ClienteDAO>();
+            services.AddHttpContextAccessor();
 
 
             //Criar ID de contexto
