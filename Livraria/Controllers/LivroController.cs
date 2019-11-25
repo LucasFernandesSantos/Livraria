df51ddb5555b6c6a330f1d800cbec32ac6be6897
Gandin13 <gandiboon@gmail.com>
2019-11-25 17:45:25 -0300

    LivroDAO & LivroController

--- Livraria/Controllers/LivroController.cs
+++ Livraria/Controllers/LivroController.cs
@@ -2,16 +2,75 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Livraria.DAO;
+using Livraria.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Livraria.Controllers
 {
     public class LivroController : Controller
     {
-       
+        private readonly LivroDAO _livroDAO;
+        public static List<Livro> listaLivro = new List<Livro>();
+        public LivroController(LivroDAO livroDAO)
+        {
+            _livroDAO = livroDAO;
+        }
+
         public IActionResult Livraria()
         {
             return View();
+        }
+
+        public IActionResult CadastrarLivro()
+        {
+            Livro livro = new Livro();
+            if (TempData["Livro"] != null)
+            {
+                livro = JsonConvert.DeserializeObject<Livro>(TempData["Livro"].ToString());
+            }
+            return View(livro);
+        }
+
+        [HttpPost]
+        public IActionResult CadastrarLivro(Livro livro)
+        {
+            livro = _livroDAO.CadastrarLivro(livro);
+            if (livro == null)
+            {
+                ModelState.AddModelError("", "Livro já cadastrado!");
+                return View(livro);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ListLivro()
+        {
+            ViewBag.DataHora = DateTime.Now;
+            if (!listaLivro.Any())
+            {
+                return View(_livroDAO.ListarTodos());
+            }
+            return View(listaLivro);
+        }
+
+        public IActionResult EditarLivro(int id)
+        {
+            var result = View(_livroDAO.BuscarPorID(id));
+            return result;
+        }
+        public IActionResult RemoverLivro(int id)
+        {
+            _livroDAO.RemoverLivro(id);
+            return RedirectToAction("ListLivro");
+        }
+
+        [HttpPost]
+        public IActionResult EditarLivro(Livro livro)
+        {
+            _livroDAO.EditarLivro(livro);
+            return RedirectToAction("ListLivro");
         }
     }
 }
--- Livraria/DAO/LivroDAO.cs
+++ Livraria/DAO/LivroDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Livraria.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,48 @@ namespace Livraria.DAO
 {
     public class LivroDAO
     {
+        private readonly Context _context;
+        public LivroDAO(Context context)
+        {
+            _context = context;
+        }
+
+        public Livro BuscarPorID(int? id)
+        {
+            return _context.Livros.Find(id);
+        }
+
+        public Livro BuscarPorNome(Livro livro)
+        {
+            return _context.Livros.Where(x => x.items.volumeInfo.Title.Equals(livro.items.volumeInfo.Title)).FirstOrDefault();
+        }
+        public Livro CadastrarLivro(Livro livro)
+        {
+            if (BuscarPorNome(livro) != null)
+            {
+                livro = null;
+                return livro;
+            }
+            _context.Livros.Add(livro);
+            _context.SaveChanges();
+            return livro;
+        }
+
+        public List<Livro> ListarTodos()
+        {
+            return _context.Livros.ToList();
+        }
+
+        public void EditarLivro(Livro livro)
+        {
+            _context.Livros.Update(livro);
+            _context.SaveChanges();
+        }
+
+        public void RemoverLivro(int id)
+        {
+            _context.Livros.Remove(BuscarPorID(id));
+            _context.SaveChanges();
+        }
     }
 }
